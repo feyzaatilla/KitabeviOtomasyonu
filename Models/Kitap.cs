4f9fe77e2ae938fc77399a6236016aa2175f004b
Feyza Atilla <atillaafeyza@gmail.com>
2026-05-26 00:19:16 +0300

    İlk commit: proje oluşturuldu

---
+++ Models/Kitap.cs
@@ -0,0 +1,13 @@
+namespace KitabeviApp.Models
+{
+    public class Kitap
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; } = string.Empty;
+        public string Yazar { get; set;} = string.Empty;
+        public string ISBN { get; set; } = string.Empty;
+        public decimal Fiyat { get; set;}
+        public int StokAdedi { get; set; }
+        public DateTime EklenmeTarihi { get; set; } = DateTime.Now;
+    }
+}
